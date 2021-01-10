f87ae943f552351e7aafa55bede26cb8ca8c1a81
Sylvester Menawar <sylvesternathan93@gmail.com>
2021-01-10 11:47:31 +0000

    added movies row component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import Banner from './components/Banner/Banner';
+import Header from './components/Header/Header';
 import Row from './components/Rows/Row';
 import requests from './requests';
 
 function App() {
   return (
     <div className="app">
-    <h1>Netflix clone</h1>
-    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-    <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Header />
+        <Banner />
+        <Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+		    <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+		    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+		    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+		    <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+		    <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
