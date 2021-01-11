e9ad3d6f6ac2388328808755d87ab39e98735e76
Sylvester Menawar <sylvesternathan93@gmail.com>
2021-01-11 17:41:59 +0000

    added movie trailer popup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,14 +16,14 @@ function Header({}) {
   }, []);
   
     return (
-         <div className={`nav ${show && "nav_black"}`}>
+         <div className={`nav ${show && "nav__black"}`}>
            <img
-             className="nav_logo"
+             className="nav__logo"
              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
              alt="Netflix Logo"
            />
            <img
-             className="nav_avatar"
+             className="nav__avatar"
              src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
              alt="Netflix Logo"
            />
